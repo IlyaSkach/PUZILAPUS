0d3acb7f54b62d98677b597d4f9c034d86c76787
Илья С <skachilya@gmail.com>
2025-03-24 18:11:18 +0200

    add mob

--- script.js
+++ script.js
@@ -23,4 +23,25 @@ document.addEventListener("DOMContentLoaded", () => {
   goalItems.forEach((item) => {
     observer.observe(item);
   });
+
+  // Добавим код для бургер-меню
+  const burgerMenu = document.querySelector(".burger-menu");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const body = document.body;
+
+  burgerMenu.addEventListener("click", () => {
+    burgerMenu.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+    body.style.overflow = body.style.overflow === "hidden" ? "" : "hidden";
+  });
+
+  // Закрываем меню при клике на ссылку
+  const navLinks = document.querySelectorAll(".nav-link");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      burgerMenu.classList.remove("active");
+      mobileMenu.classList.remove("active");
+      body.style.overflow = "";
+    });
+  });
 });
